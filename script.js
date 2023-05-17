f71344e4470d9e949c2d44821acec38ba4476614
Allyson Mitchem <allysonmitchem@gmail.com>
2023-05-17 16:32:02 -0600

    cleaned up code/prettier

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const imageContainer = document.getElementById('image-container');
-const loader = document.getElementById('loader');
+const imageContainer = document.getElementById("image-container");
+const loader = document.getElementById("loader");
 
 let ready = false;
 let imagesLoaded = 0;
@@ -8,77 +8,74 @@ let photosArray = [];
 
 // Unsplash API
 const count = 30;
-const apiKey = 'e37mtrYTbLA-Hyqea-yvnJkFQqS8Te6r3co6YNeXNQg';
-const apiUrl =`https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+const apiKey = "e37mtrYTbLA-Hyqea-yvnJkFQqS8Te6r3co6YNeXNQg";
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
 // Check if all images were loaded
-function imageLoaded(){
-    console.log('image loaded');
-    imagesLoaded++;
-    console.log(imagesLoaded)
-    if (imagesLoaded === totalImages){
-        ready = true;
-        loader.hidden = true;
-        console.log('ready =', ready);
-    }
+function imageLoaded() {
+  imagesLoaded++;
+  if (imagesLoaded === totalImages) {
+    ready = true;
+    loader.hidden = true;
+  }
 }
 
-//Helper function to set attributes on DOM elements 
-function setAttribute(element, attributes){
-    for (const key in attributes){
-        element.setAttribute(key, attributes[key]);
-    }
+//Helper function to set attributes on DOM elements
+function setAttribute(element, attributes) {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
 }
-
 
 // Create Elements for Links & Photos, add to DOM
-function displayPhotos(){
-    imagesLoaded = 0;
-    totalImages = photosArray.length;
-    console.log('total images', totalImages)
-    // Run function for each object in photosArray
-    photosArray.forEach((photo) => {
-        // Create <a> link to Unsplash
-        const item = document.createElement('a');
-        setAttribute(item, {
-            href: photo.links.html, 
-            target: '_blank',
+function displayPhotos() {
+  imagesLoaded = 0;
+  totalImages = photosArray.length;
 
-        });
-        // Create image for photo
-        const img = document.createElement('img');
-        setAttribute(img, {
-            src: photo.urls.regular,
-            alt: photo.alt_description,
-            title: photo.alt_description,
-        });
-        // Event listener, check when each is finished loading 
-        img.addEventListener('load', imageLoaded);
-        // put <img> inside <a>, then put both inside imageContainer
-        item.appendChild(img);
-        imageContainer.appendChild(item);
+  // Run function for each object in photosArray
+  photosArray.forEach((photo) => {
+    // Create <a> link to Unsplash
+    const item = document.createElement("a");
+    setAttribute(item, {
+      href: photo.links.html,
+      target: "_blank",
     });
+    // Create image for photo
+    const img = document.createElement("img");
+    setAttribute(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
+    // Event listener, check when each is finished loading
+    img.addEventListener("load", imageLoaded);
+    // put <img> inside <a>, then put both inside imageContainer
+    item.appendChild(img);
+    imageContainer.appendChild(item);
+  });
 }
 
 // Get photos from Unsplash API
 
-async function getPhotos(){
-    try {
-        const response = await fetch(apiUrl);
-        photosArray = await response.json();
-        displayPhotos();
-        
-    } catch (error){
-        // Catch error here
-    }
+async function getPhotos() {
+  try {
+    const response = await fetch(apiUrl);
+    photosArray = await response.json();
+    displayPhotos();
+  } catch (error) {
+    // Catch error here
+  }
 }
 
 // Check to see if scrolling near bottom of page, load more photos
-window.addEventListener('scroll', () => {
-    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready){
-        ready = false;
-        getPhotos();
-    }
+window.addEventListener("scroll", () => {
+  if (
+    window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
+    ready
+  ) {
+    ready = false;
+    getPhotos();
+  }
 });
 
 // On Load
